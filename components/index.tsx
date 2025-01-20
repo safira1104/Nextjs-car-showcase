227fab5a40b1d88c5d09d944436fb4ddbd0b3184
safira rusyda <143937915+safira1104@users.noreply.github.com>
2025-01-20 11:18:35 +0700

    navbar section penambahan tombol signup dengan logo dan judul logo pada navbar

--- components/index.tsx
+++ components/index.tsx
@@ -1,7 +1,11 @@
 import Hero from "./hero";
 import CustomButton from "./custom-button";
+import Navbar from "./navbar";
+import Footer from "./Footer";
 
 export{
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }
